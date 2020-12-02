9a1752dfc3f8ca0dd6a5c9a4c2bc50321db96b7c
sahil narang <32524427+sahilnar@users.noreply.github.com>
2020-12-03 03:06:18 +0530

    second commit

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,13 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('teachers', views.TeacherViewSet,basename='teachers')
-router.register('students', views.StudentViewSet)
+router.register('teachers', views.TeacherViewSet,basename='teacher')
+router.register('students', views.StudentViewSet,basename='student')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', views.RegisterAPI.as_view(), name='register'),
+    path('login/', views.LoginAPI.as_view(), name='login'),
+    path('change-password/', views.ChangePasswordView.as_view(), name='change-password'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
